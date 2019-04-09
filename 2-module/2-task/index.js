c21a9e70b881e4d013bd341fa616334dd53c5b8e
Kiryl Alexandrovich <kiryl.alexandrovich@gmail.com>
2019-04-09 15:56:59 +0300

    feat: add find in object solution

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,5 +4,41 @@
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
+
+function find(obj, value) {
+  let paths = [];
+  let index = 0;
+  function findRed(subObj, subPath) {
+
+    for (let key in subObj) {
+
+      if (paths[index] === undefined) {
+        paths[index] = key;
+      } else {
+        paths[index] = paths[index] + '.' + key;
+      }
+
+      if (subObj[key] === value) {
+        index++;
+      } else if (typeof subObj[key] != "object") {
+        delete paths[index];
+      }
+
+      if (typeof subObj[key] === "object") {
+
+        findRed(subObj[key]);
+      }
+    }
+  }
+  findRed(obj);
+
+  if (paths[index - 1] === undefined) {
+    return null;
+  } else if (paths.length === 1) {
+    return paths[0];
+  } else {
+    return paths;
+  }
 }
+
+
